37a96273e502dbc626c7beb3af402bb0859e26fc
anisanhy <joyabrianrantepasang14@gmail.com>
2024-07-04 22:01:49 +0800

    create level 1 & 2

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("This thing is friendly");
+                break;
+            case "Finish":
+                NextLevel();
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    void NextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        
+        int nextSceneIndex = currentSceneIndex + 1;
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
